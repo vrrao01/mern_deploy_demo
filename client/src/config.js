1c9a68d9d8e5b56c069375f33152b4cc5465fd70
Vignesh Rao <vigneshravirao@gmail.com>
2021-06-28 18:50:27 +0530

    :bug: Bug fixes

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,12 @@
 import pkg from '../package.json';
 
-export default {
+const endpoints = {
   development: {
     endpoint: pkg.proxy
   },
   production: {
     endpoint: window.location.hostname
   }
-}
+};
+
+export default endpoints;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const path = require('path');
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,{
+  cors:{
+    origin:"*",
+    methods:["GET","POST"]
+  }
+});
 
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
